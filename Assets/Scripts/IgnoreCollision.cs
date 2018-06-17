30a22ee824a7fb788eac9d8f89b38cd4ee338ab5
Paweł Bagadziński <pbagadzinski@outlook.com>
2018-06-17 17:39:13 -0400

    Ignore collisions between player and enemy.

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D m_otherObject;
+
+    private void Awake()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), m_otherObject, true);
+    }
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start ()
+    {
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update ()
+    {	
+	}
+}
